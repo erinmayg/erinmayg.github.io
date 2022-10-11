2e00ed45478ce821f491cb0229ac7f443d973177
erinmayg <erinmaygunawan@gmail.com>
2022-10-12 00:24:13 +0800

    Create mobile-friendly navbar

---
+++ src/components/Menu.tsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { NAVLINKS, RESUME } from '../constants'
+import IconMenu from './icons/menu'
+
+const StyledMenu = styled.div`
+  display: none;
+
+  svg {
+    width: 24px;
+    height: 24px;
+    position: relative;
+    z-index: 5;
+  }
+
+  @media (max-width: 768px) {
+    display: block;
+    position: relative;
+  }
+`
+
+const StyledSidebar = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+    position: fixed;
+    top: 0;
+    right: 0;
+
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+
+    backdrop-filter: ${(props: { isOpen: boolean }) =>
+      props.isOpen ? 'blur(10px)' : 'none'};
+
+    transition: var(--transition);
+
+    ol {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+      position: fixed;
+      padding: 2rem;
+      top: 0;
+
+      transition: var(--transition);
+      right: ${(props: { isOpen: boolean }) => (props.isOpen ? '0' : '-300px')};
+
+      background-color: var(--bgColor);
+      width: 300px;
+      height: 100%;
+
+      li {
+        list-style: none;
+        margin: 2rem;
+      }
+
+      a {
+        margin: 1rem;
+        cursor: pointer;
+        transition: var(--transition);
+
+        &:hover {
+          color: var(--accent1);
+        }
+      }
+    }
+  }
+`
+
+function Menu() {
+  const [openMenu, setOpenMenu] = useState(false)
+
+  return (
+    <StyledMenu>
+      <button
+        style={{ cursor: 'pointer' }}
+        onClick={() => setOpenMenu(!openMenu)}
+      >
+        <IconMenu />
+      </button>
+      <StyledSidebar isOpen={openMenu}>
+        <ol>
+          {NAVLINKS &&
+            NAVLINKS.map((navLink, i) => (
+              <li key={i}>
+                <a
+                  href={navLink.link}
+                  rel='noreferrer'
+                  onClick={() => setOpenMenu(false)}
+                >
+                  {navLink.name}
+                </a>
+              </li>
+            ))}
+          <li>
+            <a
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                border: '1px solid',
+                borderRadius: '8px',
+                padding: '0.8rem 1.2rem',
+              }}
+              href={RESUME}
+              target='_blank'
+              rel='noreferrer'
+            >
+              Resume
+            </a>
+          </li>
+        </ol>
+      </StyledSidebar>
+    </StyledMenu>
+  )
+}
+
+export default Menu
--- src/components/icons/icon.tsx
+++ src/components/icons/icon.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Icon = styled.svg`
   cursor: pointer;
   transition: var(--transition);
+  user-select: none;
 `
 
 export default Icon
