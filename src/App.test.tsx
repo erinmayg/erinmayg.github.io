71b8e6dcaa169c6e2e0566be1f2fddb9ce5c6f10
erinmayg <erinmaygunawan@gmail.com>
2022-10-12 00:47:23 +0800

    Update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import Nav from './components/Nav'
 import Hero from './components/sections/Hero'
-
-test('Nav render test', () => {
-  render(<Nav />)
-  expect(screen.getByText(/about/i)).toBeInTheDocument()
-  expect(screen.getByText(/experience/i)).toBeInTheDocument()
-  expect(screen.getByText(/work/i)).toBeInTheDocument()
-})
 
 test('Hero render test', () => {
   render(<Hero />)
